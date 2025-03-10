b308111483737f15519e425667626462691f7412
Yhago Carvalho <yhago.serafim@quintoandar.com.br>
2025-03-09 21:48:33 -0300

    Deploy estático para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  output: 'export',
+const nextConfig = {
   basePath: "/fiel-news",
   images: {
     unoptimized: true,
